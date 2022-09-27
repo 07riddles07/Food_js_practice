4c385e2bbf7e8fc827586e2207c1508a3a2e9b9d
Anastasia Rarova <07tangled07@gmail.com>
2022-09-27 18:53:27 +0300

    written a code for those cases when the count down is finished

--- js/script.js
+++ js/script.js
@@ -40,11 +40,21 @@ document.addEventListener('DOMContentLoaded', () => {
     const deadline = '2023-01-07';
 
     function getRemainingTime(endtime) {
-        const t = Date.parse(endtime) - Date.parse(new Date()),
+        let days, hours, minutes, seconds;
+        const t = Date.parse(endtime) - Date.parse(new Date());
+
+        if (t <= 0) {
+            days = 0;
+            hours = 0;
+            minutes = 0;
+            seconds = 0;
+        } else {
             days = Math.floor(t / (1000 * 60 * 60 * 24)),
-            hours = Math.floor((t / (1000 * 60 * 60) % 24)),
-            minutes = Math.floor((t / 1000 / 60) % 60),
-            seconds = Math.floor((t / 1000) % 60);
+                hours = Math.floor((t / (1000 * 60 * 60) % 24)),
+                minutes = Math.floor((t / 1000 / 60) % 60),
+                seconds = Math.floor((t / 1000) % 60);
+        }
+
 
         return {
             'total': t,
@@ -56,8 +66,8 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     function getZero(num) {
-        if(num >= 0 && num < 10) {
-            return`0${num}`;
+        if (num >= 0 && num < 10) {
+            return `0${num}`;
         } else {
             return num;
         }
@@ -65,11 +75,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function setTimer(selector, endtime) {
         const timer = document.querySelector(selector),
-        days = timer.querySelector('#days'),
-        hours = timer.querySelector('#hours'),
-        minutes = timer.querySelector('#minutes'),
-        seconds = timer.querySelector('#seconds'),
-        timeInterval = setInterval(updateTimer, 1000);
+            days = timer.querySelector('#days'),
+            hours = timer.querySelector('#hours'),
+            minutes = timer.querySelector('#minutes'),
+            seconds = timer.querySelector('#seconds'),
+            timeInterval = setInterval(updateTimer, 1000);
 
         updateTimer();
 
