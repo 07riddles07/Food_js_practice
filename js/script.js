104db2380873e265a326d0e273ab40480e3c4eec
Anastasia Rarova <07tangled07@gmail.com>
2022-10-10 12:52:59 +0300

    a class with menu cards is added

--- js/script.js
+++ js/script.js
@@ -145,4 +145,39 @@ document.addEventListener('DOMContentLoaded', () => {
 
     window.addEventListener('scroll', showModalWindowByScrolling);
 
+    // Classes for Menu cards
+
+    class MenuCard {
+        constructor(src, alt, title, descr, price, parentSelector) {
+            this.src = src;
+            this.alt = alt;
+            this.title = title;
+            this.descr = descr;
+            this.price = price;
+            this.parentSelector = document.querySelector(parentSelector);
+            this.convertCurrency = 63;
+            this.converttoRub();
+        }
+
+        converttoRub() {
+            this.price = this.price * this.convertCurrency;
+        }
+
+        render() {
+            const div = document.createElement('div');
+            element.innerHTML = `<div class="menu__item">
+        <img src="${this.src}" alt="${this.alt}">
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr">${this.descr}</div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+            <div class="menu__item-cost">Цена:</div>
+            <div class="menu__item-total"><span>${this.price}</span> руб/день</div>
+        </div>
+    </div>`;
+
+            this.parent.append(element);
+        }
+    }
+
 });
